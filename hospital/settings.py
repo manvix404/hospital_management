00098d6bee43ec5730f3d0614959e213202b9ddd
manvix404 <71367200+manvix404@users.noreply.github.com>
2022-02-21 21:55:25 +0530

    Update settings.py

--- hospital/settings.py
+++ hospital/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qe&ckf9d@m+u)xz$b5zk+=f!do7tx9^kjscj&_+r_x^gi90uv7'
+SECRET_KEY = Enter Django Key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -135,5 +135,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'Insert email'
-EMAIL_HOST_PASSWORD = 'insert password'
+EMAIL_HOST_USER = MailID
+EMAIL_HOST_PASSWORD = Passwd
